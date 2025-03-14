1a4f851cf5174bbab76f4a754dd9f2a773096a60
linuxfanboy4 <imlinuxfanboy@outlook.com>
2025-03-14 10:54:48 +0600

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    mainWindow.webContents.openDevTools();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
